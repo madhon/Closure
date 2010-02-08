b07e2e044af4f5f2df3b7c67014b8d6a55336000
Madhon <madhon@madhon.co.uk>
2010-02-08 14:29:26 +0000

    initial commit

---
+++ Closure/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Xml;
+
+namespace Closure
+{
+    public class Program
+    {
+        private const string Data =
+            "js_code={0}&output_format=xml&output_info=compiled_code&compilation_level=SIMPLE_OPTIMIZATIONS";
+
+        private const string Url = "http://closure-compiler.appspot.com/compile";
+
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            string sourceFile = args[0];
+
+            Console.WriteLine("Loading " + sourceFile);
+            string content = File.ReadAllText(sourceFile);
+            WebClient wClient = new WebClient();
+            wClient.Headers.Add("content-type", "application/x-www-form-urlencoded");
+            string apiData = string.Format(Data, HttpUtility.UrlEncode(content));
+
+            Console.WriteLine("Processing");
+            string response = wClient.UploadString(Url, apiData);
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(response);
+
+            Console.WriteLine("Writing compressed-" + sourceFile);
+            File.WriteAllText("compressed-" + sourceFile, xml.SelectSingleNode("//compiledCode").InnerText);
+        }
+    }
+}
---
+++ Closure/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Closure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Madhon")]
+[assembly: AssemblyProduct("Closure")]
+[assembly: AssemblyCopyright("Copyright © Madhon 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: Guid("034002f6-00c4-4cdd-a60c-6259317ee90f")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.10032.1722")]
